7e7237418b4c62a17daeda0940456d240c78b7c3
RustamSaydam <rustamsaydam@icloud.com>
2023-06-07 20:44:12 +0300

    1.7 Added Admin panel

--- src/main/java/com/example/sellbuy/controllers/UserController.java
+++ src/main/java/com/example/sellbuy/controllers/UserController.java
@@ -4,7 +4,9 @@ import com.example.sellbuy.models.User;
 import com.example.sellbuy.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -33,5 +35,16 @@ public class UserController {
     public String securityUrl() {
         return "hello";
     }
+
+    @GetMapping("/user/{user}")
+    public String userInfo(@PathVariable("user") User user, Model model) {
+        model.addAttribute("user", user);
+        model.addAttribute("products", user.getProducts());
+        return "user-info";
+    }
+
+
 }
+
+
 
